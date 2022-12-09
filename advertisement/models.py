80050d3fc2a8a53519a2e4a813c169dea3dc95ec
Rakhat Zhumaev <livencor@gmail.com>
2022-12-09 14:28:11 +0600

    added advertisement models

--- advertisement/models.py
+++ advertisement/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from user.models import CustomUser
+from cities.models import BaseCountry
 
 
 def get_upload_path_ad_image(instance, filename):
@@ -10,11 +11,27 @@ def get_upload_path_head_image(instance, filename):
     return "ad_images/{username}/{file}".format(username=instance.username(), file=filename)
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+
+
+class SubCategory(models.Model):
+    title = models.CharField(max_length=50)
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, related_name='sub_category')
+
+
 class Advertisement(models.Model):
     title = models.CharField(max_length=255)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     description = models.TextField()
     head_image = models.FileField(upload_to=get_upload_path_head_image, blank=True, null=True)
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+    max_price = models.PositiveIntegerField()
+    views = models.PositiveIntegerField()
+    city = BaseCountry()
+    end_date = models.DateTimeField()
+    created_on = models.DateTimeField()
 
     def username(self):
         return self.user.username
@@ -26,3 +43,22 @@ class Advertisement(models.Model):
 class AdvertisementImage(models.Model):
     advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
     image = models.FileField(upload_to=get_upload_path_ad_image, blank=True, null=True)
+
+
+class AdvertisementComment(models.Model):
+    parent_id = models.IntegerField()
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='comments')
+    advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+
+
+class AdvertisementPromotion(models.Model):
+    advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE, related_name='promotion')
+
+
+class Promotion(models.Model):
+    name = models.CharField()
+    price = models.FloatField()
+    advertisement_promotion = models.ForeignKey(AdvertisementPromotion,
+                                                on_delete=models.CASCADE,
+                                                related_name='promotion')
---
+++ advertisement/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from advertisement.models import Advertisement
+
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Advertisement
+        fields = '__all__'
+
