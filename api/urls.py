7663c94f99fd4037867f904c8661e184a59a5847
Altynai Mamytova <altynai.mamytova17@gmail.com>
2022-12-20 21:49:24 +0000

    JWT generation completed

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.RegisterApi.as_view(), name='api-register'),
-    path('api/token/', views.TokenObtainView.as_view(), name='api-token-obtain'),
-    path('api/token/refresh', views.TokenRefreshView.as_view(), name='api-token-refresh')
-]
