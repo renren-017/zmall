bb900956ac6dfec92f1e461a0ef0ceda7cf5f45b
Altynai Mamytova <altynai.mamytova17@gmail.com>
2023-01-05 08:57:40 +0000

    Redis attached

---
+++ core/middleware/viewcounter.py
@@ -0,0 +1,11 @@
+from core.db_management.connections.redis_conn import redis_db
+
+
+class ViewCountMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
